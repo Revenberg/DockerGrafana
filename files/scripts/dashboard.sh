06321bb5ac0ff0c114c0076223d0e651574e5a52
Sander Revenberg <sander@revenberg.info>
2021-08-01 19:27:06 +0200

    .

--- files/scripts/dashboard.sh
+++ files/scripts/dashboard.sh
@@ -1,15 +1,16 @@
 readonly URL=${URL:-"http://localhost:3000"}
 readonly LOGIN=${LOGIN:-"admin:admin"}
 
-inp=$1
-inp="/etc/grafana/provisioning/dashboards/grafana-gas.json"
+map="/etc/grafana/provisioning/dashboards"
 
-dashboard=$(cat $inp | jq ' .id = null' )
+for f in $map
+do
+    inp="/etc/grafana/provisioning/dashboards/$f"
 
-echo $dashboard
+    dashboard=$(cat $inp | jq ' .id = null' )
 
-data=$(echo -e "{\n    \"dashboard\": $dashboard,\n    \"overwrite\": true,\n    \"message\": \"Updated by ansible\"\n}\n")
 
-echo $data
+    data=$(echo -e "{\n    \"dashboard\": $dashboard,\n    \"overwrite\": true,\n    \"message\": \"Updated by ansible\"\n}\n")
 
-curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/dashboards/db -XPOST -d "$data"
+    curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/dashboards/db -XPOST -d "$data"
+done
