c1e63e0cff3628b57c0656db0b6041b494f00630
Sander Revenberg <sander@revenberg.info>
2021-08-01 22:26:23 +0200

    .

--- files/scripts/dashboard.sh
+++ files/scripts/dashboard.sh
@@ -14,5 +14,5 @@ do
 
     data=$(echo -e "{\n    \"dashboard\": $dashboard,\n    \"overwrite\": true,\n    \"message\": \"Updated by init\"\n}\n")
 
-    curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/dashboards/db -XPOST -d "$data"
+    curl -s --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/dashboards/db -XPOST -d "$data"
 done
