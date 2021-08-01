4fd93ceb96a12115b24acd6c32f323b0a30a598c
Sander Revenberg <sander@revenberg.info>
2021-08-01 22:27:14 +0200

    .

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ chmod +x build.sh
 
 docker image build -t revenberg/grafana . 
 
-docker run -p 3000:3000 revenberg/grafana
+#docker run -p 3000:3000 revenberg/grafana
 
-#docker push revenberg/grafana
+docker push revenberg/grafana
