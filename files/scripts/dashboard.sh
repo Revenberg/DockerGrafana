067fb07bfe837ff9cd1dfc2a4a157a1ea3a97cb8
Sander Revenberg <sander@revenberg.info>
2021-08-01 19:10:16 +0200

    .

--- files/scripts/dashboard.sh
+++ files/scripts/dashboard.sh
@@ -3,4 +3,6 @@ readonly LOGIN=${LOGIN:-"admin:admin"}
 
 dashboard=$(cat $1 | jq ' .id = null' )
 
-curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/dashboards/db -XPOST -d "$dashboard"
+data=${echo -e "{\n    \"dashboard\": $dashboard,\n    \"overwrite\": true,\n    \"message\": \"Updated by ansible\"\n}\n"}
+
+curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/dashboards/db -XPOST -d "$data"
