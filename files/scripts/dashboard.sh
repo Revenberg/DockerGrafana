95f884fc6e76056359d8478ba45e9424ad5fa509
Sander Revenberg <sander@revenberg.info>
2021-08-01 18:56:55 +0200

    .

--- files/scripts/dashboard.sh
+++ files/scripts/dashboard.sh
@@ -1,6 +1,6 @@
 readonly URL=${URL:-"http://localhost:3000"}
 readonly LOGIN=${LOGIN:-"admin:admin"}
 
-dashboard=$(cat $1)
+dashboard=$(cat $1 | jq ' .id = null' )
 
 curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/dashboards/db -XPOST -d "$dashboard"
