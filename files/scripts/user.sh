7fcdb19c6b3d7526092bb8c4f9ca149709439f46
Sander Revenberg <sander@revenberg.info>
2021-08-12 15:34:38 +0200

    admin

--- files/scripts/user.sh
+++ files/scripts/user.sh
@@ -4,11 +4,11 @@ echo "import dashboards task"
 sleep 50s
 
 readonly URL=${URL:-"http://localhost:3000"}
-readonly LOGIN=${LOGIN:-"pi:pi"}
+readonly LOGIN=${LOGIN:-"admin:admin"}
 
 echo "add user"
 
 passwrd=$(cat /home/pi/.pswrd)
-data="name=&username=pi&email=pi@locahome&password=$passwrd&active=true,approved=true"
+data="name=admin&username=admin&email=admin@locahome&password=admin&active=true,approved=true"
 
 curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users -XPOST -d "$data"
