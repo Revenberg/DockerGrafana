88ec651f8d83da0d11d6cf695c11a09872e3a8e8
Sander Revenberg <sander@revenberg.info>
2021-11-22 08:43:41 +0100

    .

--- files/scripts/user.sh
+++ files/scripts/user.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -e
 
-echo "import dashboards task"
+echo "import users task"
 sleep 30s
 
 readonly URL=${URL:-"http://localhost:3000"}
 readonly LOGIN=${LOGIN:-"admin:admin"}
-
-data="name=admin&username=admin&email=admin@localhome&password=admin&active=true,approved=true"
-curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users -XPOST -d "$data"
 
 echo "add adminuser"
 data='{ "name": "adminuser", "username": "adminuser", "email": "adminuser@localhome", "password": "adminuser", "active": "true", "approved": "true"}'
