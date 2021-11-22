428736ae5269e33afe763fea0d332d491e4b9f90
Sander Revenberg <sander@revenberg.info>
2021-11-22 10:01:02 +0100

    .

--- files/scripts/user.sh
+++ files/scripts/user.sh
@@ -7,12 +7,12 @@ URL=${URL:-"http://localhost:3000"}
 LOGIN=${LOGIN:-"admin:admin"}
 
 echo "add adminuser"
-data='{ "name": "adminuser", "username": "adminuser", "email": "adminuser@localhome", "password": "adminuser", "active": "true", "approved": "true"}'
+data='{ "name": "adminuser", "login": "adminuser", "email": "adminuser@localhome", "password": "adminuser", "active": "true", "approved": "true", "isAdmin": "true"}'
 curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users -XPOST -d "$data"
 
 echo "add pi"
 passwrd=$(cat /usr/share/.pswrd)
-data='{ "name": "pi", "username": "pi", "email": "pi@localhome", "password": "'$passwrd'", "active": "true", "approved": "true"}'
+data='{ "name": "pi", "login": "pi", "email": "pi@localhome", "password": "'$passwrd'", "active": "true", "approved": "true", "isAdmin": "true"}'
 curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users -XPOST -d "$data"
 
 
