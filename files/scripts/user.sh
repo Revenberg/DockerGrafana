5052444c06a3366478585b1110272320d64d4193
Sander Revenberg <sander@revenberg.info>
2021-11-22 11:12:34 +0100

    .

--- files/scripts/user.sh
+++ files/scripts/user.sh
@@ -6,12 +6,10 @@ sleep 30s
 LOGIN=${LOGIN:-"admin:admin"}
 
 echo "add adminuser"
-data='{ "name": "adminuser", "login": "adminuser", "email": "adminuser@localhome", "password": "adminuser", "active": "true", "approved": "true", "isAdmin": "true"}'
+data='{ "name": "adminuser", "login": "adminuser", "email": "adminuser@localhome", "password": "adminuser", "active": true, "approved": true, "isGrafanaAdmin": true}'
 curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users -XPOST -d "$data"
 
 echo "add pi"
 passwrd=$(cat /usr/share/.pswrd)
-data='{ "name": "pi", "login": "pi", "email": "pi@localhome", "password": "'$passwrd'", "active": "true", "approved": "true", "isAdmin": "true"}'
+data='{ "name": "pi", "login": "pi", "email": "pi@localhome", "password": "'$passwrd'", "active": true, "approved": true, "isGrafanaAdmin": true}'
 curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users -XPOST -d "$data"
-
-
