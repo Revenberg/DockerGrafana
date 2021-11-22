c2b1c7c0ab76dd929ac515d544d62914f7d22e1b
Sander Revenberg <sander@revenberg.info>
2021-11-22 12:26:33 +0100

    .

--- files/scripts/user.sh
+++ files/scripts/user.sh
@@ -7,13 +7,21 @@ LOGIN=${LOGIN:-"admin:admin"}
 
 echo "add adminuser"
 data='{ "name": "adminuser", "login": "adminuser", "email": "adminuser@localhome", "password": "adminuser", "active": true, "approved": true }'
-curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users -XPOST -d "$data"
 
-data='{"isGrafanaAdmin": true}'
-curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users/17/permissions -XPOST -d "$data"
+rc=$( curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users -XPOST -d "$data" )
+id=$(echo $rc | cut -d':' -f2 | cut -d',' -f1)
+
+data='{"role":"Admin"}'
+curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/orgs/1/users/$id -XPATCH -d "$data"
 
 
 echo "add pi"
 passwrd=$(cat /usr/share/.pswrd)
 data='{ "name": "pi", "login": "pi", "email": "pi@localhome", "password": "'$passwrd'", "active": true, "approved": true, "isGrafanaAdmin": true}'
 curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users -XPOST -d "$data"
+
+rc=$( curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/admin/users -XPOST -d "$data" )
+id=$(echo $rc | cut -d':' -f2 | cut -d',' -f1)
+
+data='{"role":"Admin"}'
+curl --user "$LOGIN" -H 'Content-Type: application/json' http://localhost:3000/api/orgs/1/users/$id -XPATCH -d "$data"
